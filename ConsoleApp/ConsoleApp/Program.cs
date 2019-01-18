ee13b295c29fb4809e0ac347361d8c35ed8a96d3
André P. Bertoletti <apbertoletti@gmail.com>
2019-01-18 18:55:04 -0200

    Implement mult method

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -9,6 +9,12 @@ namespace ConsoleApp
             Console.WriteLine("Hello World!");
 
             var r = Sum(1m, 2m);
+            var b = Mult(2m, 5m);
+        }
+
+        static object Mult(decimal v1, decimal v2)
+        {
+            return v1 * v2;
         }
 
         static decimal Sum(decimal a, decimal b)
