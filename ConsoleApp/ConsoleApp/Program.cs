b82f2a440bf44114e727bbb859f6c7ee79c763df
André P. Bertoletti <apbertoletti@gmail.com>
2019-01-18 19:10:08 -0200

    New sub method

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp
 
             var r = Sum(1m, 2m);
             var b = Mult(2m, 5m);
+            var c = Sub(3m, 2m);
+        }
+
+        static object Sub(decimal v1, decimal v2)
+        {
+            return v1 - v2;
         }
 
         static object Mult(decimal v1, decimal v2)
