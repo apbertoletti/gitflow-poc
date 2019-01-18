eadc395fecbc8ab2b60dc8770aabf34cc175aa1b
André P. Bertoletti <apbertoletti@gmail.com>
2019-01-18 18:53:51 -0200

    Implement the sum method (feature AC-557)

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,6 +7,13 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var r = Sum(1m, 2m);
+        }
+
+        static decimal Sum(decimal a, decimal b)
+        {
+            return a + b;
         }
     }
 }
